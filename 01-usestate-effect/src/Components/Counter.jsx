cde11bb25b48cad0f414c035d56f4c67769c439b
arth1312 <arthkoradiya@gmail.com>
2025-06-13 18:35:50 +0530

    01-usestate-effect 13/06/2025

---
+++ 01-usestate-effect/src/Components/Counter.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0);
+
+    const handalClick = () => {
+        setCount(count => count + 1)
+    }
+
+    const handalDec = () => {
+        setCount(count => count - 1)
+    }
+
+    // Everytime
+    // useEffect(() => {
+    //     console.log("Mouting");
+    // })
+
+    // Only Once
+    // useEffect(() => {
+    //     console.log("Only Once Mouting");
+    // }, [])
+
+    // Updation
+    useEffect(() => {
+        console.log("Updation Mouting");
+    }, [count])
+
+    return (
+        <div>
+            <h1>Counter App : {count}</h1>
+            <button onClick={handalClick}>Increment</button>
+            <button onClick={handalDec}>Decrement</button>
+        </div>
+    )
+}
+export default Counter
