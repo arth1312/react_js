53d47873d7f60785780424b9f813f184350b715c
arth1312 <arthkoradiya@gmail.com>
2025-07-11 18:43:13 +0530

    11-react-routing 11/07/2025

--- 11-react-routing/src/Components/Header.jsx
+++ 11-react-routing/src/Components/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router'
 const Header = () => {
     return (
         <>
-            <Link to={"/"}>controllcomp</Link> &nbsp;&nbsp;
-            <Link to={"/uncontrollcomp"}>uncontrollcomp</Link> &nbsp;&nbsp;
-            <Link to={"/counter"}>counter</Link> &nbsp;&nbsp;
-            <Link to={"/events"}>events</Link> &nbsp;&nbsp;
-            <Link to={"/validation"}>validation</Link>
+            <Link to={"/"} className='text-decoration-none'>controllcomp</Link> &nbsp;&nbsp;
+            <Link to={"/uncontrollcomp"} className='text-decoration-none'>uncontrollcomp</Link> &nbsp;&nbsp;
+            <Link to={"/counter"} className='text-decoration-none'>counter</Link> &nbsp;&nbsp;
+            <Link to={"/events"} className='text-decoration-none'>events</Link> &nbsp;&nbsp;
+            <Link to={"/validation"} className='text-decoration-none'>validation</Link>
         </>
     )
 }
