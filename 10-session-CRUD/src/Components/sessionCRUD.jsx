f34dac193fe1673f91f840935521f81fc0fb90a2
arth1312 <arthkoradiya@gmail.com>
2025-07-11 10:08:33 +0530

    10-session-CRUD 11/07/2025

--- 10-session-CRUD/src/Components/sessionCRUD.jsx
+++ 10-session-CRUD/src/Components/sessionCRUD.jsx
@@ -1,84 +1,76 @@
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import './sessionCRUD.css'
-import generateUniqueId from 'generate-unique-id';
-import BookDetail from "./BookData";
+import { useEffect, useState } from "react";
+// import generateUniqueId from 'generate-unique-id';
 
-const getSotrageData = () => {
-    return JSON.parse(sessionStorage.getItem("Books")) || []
+const getStorageData = () => {
+    const storedBooks = localStorage.getItem("books");
+    if (!storedBooks) return [];
+    return JSON.parse(storedBooks);
 }
 
 const Books = () => {
-    const intialState = {
-        id: "",
-        title: "",
-        desc: "",
-        price: "",
-        category: "",
-        image: ""
+    const intsialstate = {
+        name: " ",
+        id: " ",
+        desc: " ",
+        price: " ",
+        category: " ",
+        image: " ",
     }
-    const [inputForm, setInputForm] = useState(intialState);
-    const [bookData, setBookData] = useState(getSotrageData());
-    const [isEdit, setIsEdit] = useState(false)
+    const [inputForm, setInputForm] = useState(intsialstate);
+    const [storageData, setStorageData] = useState(getStorageData())
+    const [isEdit, setisEdit] = useState(false)
 
-    const handleChanged = (e) => {
+    const handleInput = (e) => {
         const { name, value } = e.target;
+
         setInputForm({
             ...inputForm,
             [name]: value
-        })
+        });
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault();
         if (isEdit) {
-            let updateData = bookData.map(book => {
-                if (book.id == inputForm.id) {
-                    return inputForm
-                } else {
-                    return book
-                }
-            })
-
-            setBookData(updateData);
-            setIsEdit(false);
-        } else {
-            let id = generateUniqueId({
-                length: 6,
-                useLetters: false
-            });
-            inputForm.id = id
-            setBookData([...bookData, inputForm]);
+            setStorageData((prevData) =>
+                prevData.map((book) => book.id == inputForm.id ? inputForm : book)
+            )
         }
-        setInputForm(intialState);
+        else {
+            e.preventDefault();
+            let id = Math.floor(Math.random() * 1000)
+            setStorageData([...storageData, { ...inputForm, id }])
+        }
+        setInputForm(intsialstate);
+        setisEdit(false);
     }
 
     const handleDelete = (id) => {
-        let updatedData = bookData.filter(book => book.id != id)
-        setBookData(updatedData);
+        setStorageData(storageData.filter((book) => book.id !== id))
     }
 
-    const handleEdit = (id) => {
-        let signleRec = bookData.find(book => book.id == id)
-        setInputForm(signleRec);
-        setIsEdit(true);
+    const handleEdit = (book) => {
+        setInputForm(book)
+        setisEdit(true)
     }
 
     useEffect(() => {
-        sessionStorage.setItem("Books", JSON.stringify(bookData));
-    }, [bookData])
+        localStorage.setItem("books", JSON.stringify(storageData))
+    }, [storageData])
 
     return (
         <>
             <Container>
                 <h1>Add Book</h1>
                 <div className="form-wrapper">
-                    <Form>
+                    <Form className=" form " onSubmit={handleSubmit}>
                         <Form.Group as={Row} className="mb-3" >
                             <Form.Label column sm="2">
-                                Title
+                                Book Name
                             </Form.Label>
                             <Col sm="9">
-                                <Form.Control type="text" placeholder="Enter Title" />
+                                <Form.Control type="text" placeholder="Enter Book Name" name="name" value={inputForm.name} onChange={handleInput} />
                             </Col>
                         </Form.Group>
 
@@ -87,16 +79,16 @@ const Books = () => {
                                 Description
                             </Form.Label>
                             <Col sm="9">
-                                <Form.Control type="text" placeholder="Enter Description" />
+                                <Form.Control type="text" placeholder="Enter Book Description" name="desc" value={inputForm.desc} onChange={handleInput} />
                             </Col>
                         </Form.Group>
 
                         <Form.Group as={Row} className="mb-3" >
                             <Form.Label column sm="2">
-                                Price
+                                Book Price
                             </Form.Label>
                             <Col sm="9">
-                                <Form.Control type="number" placeholder="Enter Price" />
+                                <Form.Control type="number" placeholder="Enter Book Price" name="price" value={inputForm.price} onChange={handleInput} />
                             </Col>
                         </Form.Group>
 
@@ -105,25 +97,25 @@ const Books = () => {
                                 Category
                             </Form.Label>
                             <Col sm="9">
-                                <Form.Select aria-label="Default select example" name="category">
+                                <Form.Select aria-label="Default select example" name="category" value={inputForm.category} onChange={handleInput}>
                                     <option>Select Category</option>
-                                    <option value="Comic">Comic</option>
-                                    <option value="Story">Story</option>
-                                    <option value="History">History</option>
+                                    <option value="Comic" selected={inputForm.category == "Comic"}>Comic</option>
+                                    <option value="Story" selected={inputForm.category == "Story"}>Story</option>
+                                    <option value="History" selected={inputForm.category == "History"}>History</option>
                                 </Form.Select>
                             </Col>
                         </Form.Group>
 
                         <Form.Group as={Row} className="mb-3" >
                             <Form.Label column sm="2">
-                                Image
+                                Book URL
                             </Form.Label>
                             <Col sm="9">
-                                <Form.Control type="text" placeholder="Enter Image URL" />
+                                <Form.Control type="text" placeholder="Enter Image URL" name="image" value={inputForm.image} onChange={handleInput} />
                             </Col>
                         </Form.Group>
 
-                        <Button type="submit">Add Book</Button>
+                        <Button type="submit">{isEdit ? "Update Book" : "Add Book"}</Button>
                     </Form>
                 </div>
             </Container>
@@ -131,12 +123,32 @@ const Books = () => {
             <hr />
             <Container>
                 <h1>View Data</h1>
-                <div className="d-flex">
-                    {
-                        bookData.map(book => (
-                            <BookDetail key={book.id} book={book} handleDelete={handleDelete} handleEdit={handleEdit} />
-                        ))
-                    }
+                <div className="cards">
+                    {storageData.map((book) => (
+                        <div className="card" key={book.id}>
+                            <div className="card-img">
+                                <img src={book.image} alt="book-img" />
+                            </div>
+                            <div className="card-info">
+                                <h1>{book.name}</h1>
+                                <p>{book.desc}</p>
+                                <div className="book-price">
+                                    <p>{book.price}</p>
+                                </div>
+                                <div className="book-category">
+                                    <p>{book.category} </p>
+                                </div>
+                                <div className="edit-delete-btn">
+                                    <div className="edit-btn">
+                                        <button type="submit" onClick={() => handleEdit(book)}>Edit</button>
+                                    </div>
+                                    <div className="delete-btn">
+                                        <button type="submit" onClick={() => handleDelete(book.id)}>Delete</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </Container>
         </>
